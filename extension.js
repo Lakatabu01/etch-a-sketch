a4f736bf2581361644f62e79829614216918740f
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2022-02-19 02:35:55 -0500

    All functionalities put in place

--- extension.js
+++ extension.js
@@ -22,13 +22,15 @@ function changeSize(input){
     if (input >= 2 && input <= 100){
     populateSize(input)
     } else {
-        console.log("Too many squares")
+        alert("Pick a number between 2 and 100")
     }
 }
 
 function changeSquareColor() {
 if (color === 'random'){
     this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`   
+    }else if (color === 'pickedColor'){
+        this.style.backgroundColor = colorInput.value
     } else{
     this.style.backgroundColor = color
     }
@@ -43,3 +45,17 @@ function resetBoard(){
     let squares = board.querySelectorAll("div")
     squares.forEach((div) => div.style.backgroundColor = "white")   
 }
+
+let buttons = document.querySelectorAll("button")
+buttons.forEach((buttons) => buttons.addEventListener("mouseover", () => buttons.classList.add("scale")))
+buttons.forEach((buttons) => buttons.addEventListener("mouseleave", () => buttons.classList.remove("scale")))
+
+let placeholder = document.getElementById("placeholder")
+placeholder.addEventListener("keydown", () => placeholder.classList.add("scale"))
+placeholder.addEventListener("keyup", () => placeholder.classList.remove("scale"))
+
+let colorInput = document.querySelector("#color")
+colorInput.addEventListener("input", () => {
+    let pickedColor = colorInput.value
+    changeColor(pickedColor)
+})
